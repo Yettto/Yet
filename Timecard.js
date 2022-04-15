777a4491aa72bd72331025db71bab0ac1f08ce31
Yettto <100932205+Yettto@users.noreply.github.com>
2022-04-15 23:06:54 +0800

    Update Timecard.js

--- Timecard.js
+++ Timecard.js
@@ -75,7 +75,7 @@ function datenotice() {
   }
 }
 $done({
-title:"节假日倒计时",
+title:"节日倒计时",
 icon:"filemenu.and.selection",
 'icon-color': "#2F4F4F",
 content:tlist[nowlist][0]+"  :  "+today(tnumcount(nowlist))+"天\n"+tlist[Number(nowlist) + Number(1)][0] +"  :  "+ tnumcount(Number(nowlist) + Number(1))+ "天\n"+tlist[Number(nowlist) + Number(2)][0]+"  :  "+tnumcount(Number(nowlist) + Number(2))+"天"
