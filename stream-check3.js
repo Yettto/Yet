40e8e1bb386b6cb5ebc95494493a935cbbaede52
Yettto <100932205+Yettto@users.noreply.github.com>
2022-08-18 23:33:55 +0800

    Update and rename stream-check2.js to stream-check3.js

--- stream-check3.js
+++ stream-check3.js
@@ -167,7 +167,7 @@ panel_result['content'] = content
       })
     }
   
-     let netflix_check_result = 'Netflix  : '
+     let netflix_check_result = 'Netflix   : '
   
     await inner_check(81215567)
       .then((code) => {
