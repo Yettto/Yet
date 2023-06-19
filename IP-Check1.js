8e4599fbb853df006ef4d086b2bfff4ea763559c
Yettto <100932205+Yettto@users.noreply.github.com>
2023-06-19 13:16:10 +0800

    Create IP-Check1.js

---
+++ IP-Check1.js
@@ -0,0 +1,30 @@
+let url = "http://ip-api.com/json/?lang=zh-CN"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let citys = jsonData.regionName
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  body = {
+    title: "节点信息",
+    content: `IP信息：${ip}\n运营商：${isp}\n所在地：${emoji}｜${country} - ${city},${citys}`,
+    icon: "bonjour",
+           "icon-color": "#3A8FB7",
+  }
+  $done(body);
+});
+
+function getFlagEmoji(countryCode) {
+      if (countryCode.toUpperCase() == 'TW') {
+    countryCode = 'CN'
+  }
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char => 127397 + char.charCodeAt())
+  return String.fromCodePoint(...codePoints)
+
+}
